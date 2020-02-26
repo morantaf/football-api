7b56b9a7e529de250858f76fcac7b27e54a6272b
morantaf <fall_moranta@live.fr>
2020-02-26 16:30:51 +0100

    Setting up the database

---
+++ db.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:football@localhost:5432/postgres";
+
+const db = new Sequelize(databaseUrl);
+
+db.sync()
+  .then(() => console.log("Database synchronized"))
+  .catch(err => console.error(err));
+
+module.export = db;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log("App running on port:", port));
