d64727e1cd9250903f222a60c3b681a2027aafdd
morantaf <fall_moranta@live.fr>
2020-02-26 17:29:26 +0100

    Set up the post endpoint on the /team route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const teamRouter = require("./team/router");
-
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log("App running on port:", port));
 
+app.use(jsonParser);
 app.use(teamRouter);
--- team/router.js
+++ team/router.js
@@ -9,4 +9,13 @@ router.get("/team", (req, res, next) => {
     .catch(next);
 });
 
+router.post("/team", (req, res, next) => {
+  console.log("post request body", req.body);
+  Team.create(req.body)
+    .then(team => {
+      res.json(team);
+    })
+    .catch(next);
+});
+
 module.exports = router;
